a9011ead801e3c03f3468d16668a6d3cdbb2c46a
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-10-27 16:13:52 -0600

    add: uso de useMemo en los componente de Hero y Hero List

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,13 +1,16 @@
+import { useMemo } from "react"
 import { getHeroesBy } from "../../helpers/getHeroesBy"
 import { HeroCard } from "./HeroCard"
 
 
 export const HeroList = ({ publisher }) => {
 
+    const heroes = useMemo(()=>getHeroesBy(publisher),[publisher]);
+
     return (
         <div className='row rows-cols-1 row-cols-md-3 g-3'>
             {
-                getHeroesBy(publisher).map(heroe => (
+                heroes.map(heroe => (
                     <HeroCard key={heroe.id} {...heroe} />
                 ))
             }
