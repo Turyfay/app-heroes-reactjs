b3b59bc724d48d214d8bce3e277647f7c5b6579c
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-10-27 22:43:56 -0600

    update: logica de el component search

--- src/heroes/screens/SearchPage.jsx
+++ src/heroes/screens/SearchPage.jsx
@@ -1,17 +1,43 @@
+import { useLocation, useNavigate } from "react-router-dom"
+import { useForm } from "../../hooks/useForm"
 import { CardApp } from "../../shared/components/CardApp"
+
+import queryString from 'query-string';
+
 import { HeroCard } from "../components/HeroCard"
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+
+  const {q = ''} = queryString.parse(location.search);
+
+  const {searchText,onInputChange,onResetForm} = useForm({
+    searchText: ''
+  })
+
+  const onSubmitForm = (e)=>{
+    e.preventDefault();
+    if(searchText.trim().length <= 1) return;
+    onResetForm();
+    navigate(`?q=${searchText.toLowerCase().trim()}`);
+  }
+
+
   return (
     <CardApp title='Busqueda' subTitle="Busca a tu heroe favorito">
       <div className="row">
         <div className="col-5">
-          <form>
+          <form onSubmit={onSubmitForm}>
             <input type="text" 
             placeholder="Buscar heroe"
             className="form-control"
             name="searchText"
             autoComplete='off'
+            value={searchText}
+            onChange={onInputChange}
             />
             <button type="submit" className="btn btn-outline-primary mt-1">Buscar</button>
           </form>
@@ -23,7 +49,7 @@ export const SearchPage = () => {
             hero
           </div>
           <div className="alert alert-danger">
-            hero
+            No se encontro <b>{q}</b>
           </div>
           {/* <HeroCard/> */}
 
